f46818e397370e5ecfd3a8c19a10cac837928d30
gabriela2 <38556267+gabriela2@users.noreply.github.com>
2020-01-12 15:23:52 +0200

    Delete test-form.php

--- test-form.php
+++
@@ -1,9 +0,0 @@
-<?php
-$nume = $_POST['nume'];
-$email = $_POST['email'];
-$password = $_POST['pasword'];
-
-echo "Nume = $nume";
-echo "<br />E-mail = $email";
-echo "<br />Parola = $password";
-?>
